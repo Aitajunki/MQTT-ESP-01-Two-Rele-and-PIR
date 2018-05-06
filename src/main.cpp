539dc74bc9fdedebf55858b9a039662b7d139e99
Juan Carlos Aitajunki <aitajunki@gmail.com>
2018-05-06 15:57:32 +0200

    Se añade un sensor PIR, y se modifica el punto de acceso.

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,28 @@
     - File > Examples > PubSubClient > mqtt_esp8266
     - https://learn.adafruit.com/pir-passive-infrared-proximity-motion-sensor/using-a-pir
 
-     Configuration (HA) :
-      rele1:
-        platform: mqtt
-        name: 'Office rele1'
-        state_topic: 'office/rele1/status'
-        command_topic: 'office/rele1/switch'
-        optimistic: false
+  Configuration (HA) :
+    rele1:
+      platform: mqtt
+      name: 'Habitacion aitas (Rele1)'
+      state_topic: 'habitacion_aitas/rele1/status'
+      command_topic: 'habitacion_aitas/rele1/switch'
+      optimistic: false
 
-     Configuration (HA) :
-      rele2:
-        platform: mqtt
-        name: 'Office rele2'
-        state_topic: 'office/rele2/status'
-        command_topic: 'office/rele2/switch'
-        optimistic: false
+   Configuration (HA) :
+    rele2:
+      platform: mqtt
+      name: 'Habitacion aitas (Rele2)'
+      state_topic: 'habitacion_aitas/rele2/status'
+      command_topic: 'habitacion_aitas/rele2/switch'
+      optimistic: false
+
+    Configuration (HA) :
+      binary_sensor:
+       platform: mqtt
+       name: 'Habitacion aitas (PIR)'
+       state_topic: 'habitacion_aitas/motion/status'
+       sensor_class: motion
 
    Samuel M. - v1.1 - 08.2016
    If you like this example, please add a star! Thank you!
@@ -36,33 +43,35 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-// #include <OneButton.h>
 
 #define MQTT_VERSION MQTT_VERSION_3_1_1
 
 // Wifi: SSID and password
-const PROGMEM char* WIFI_SSID = "HomeAssistantMQTT";
+// const PROGMEM char* WIFI_SSID = "HomeAssistantMQTT";
+const PROGMEM char* WIFI_SSID = "ApBelkinWIFi";
 const PROGMEM char* WIFI_PASSWORD = "junkilin";
 
 // MQTT: ID, server IP, port, username and password
-const PROGMEM char* MQTT_CLIENT_ID = "office";
+const PROGMEM char* MQTT_CLIENT_ID = "office_aitas";
 const PROGMEM char* MQTT_SERVER_IP = "192.168.0.104";
 const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
 const PROGMEM char* MQTT_USER = "homeassistant";
 const PROGMEM char* MQTT_PASSWORD = "raspberry";
 
 // MQTT: topic
-// const PROGMEM char* MQTT_MOTION_STATUS_TOPIC = "office/motion/status";
 const char* MQTT_RELE1_STATE_TOPIC = "office/rele1/status";
 const char* MQTT_RELE1_COMMAND_TOPIC = "office/rele1/switch";
 const char* MQTT_RELE2_STATE_TOPIC = "office/rele2/status";
 const char* MQTT_RELE2_COMMAND_TOPIC = "office/rele2/switch";
+const char* MQTT_MOTION_STATUS_TOPIC = "office/motion/status";
 
 // default payload
 const char* RELE1_ON = "ON";
 const char* RELE1_OFF = "OFF";
 const char* RELE2_ON = "ON";
 const char* RELE2_OFF = "OFF";
+const char* MOTION_ON = "OFF";
+const char* MOTION_OFF = "ON";
 
 // RELE1 : GPIO0
 const PROGMEM uint8_t RELE1_PIN = 0;
@@ -71,6 +80,11 @@ boolean m_rele1_state = false; // rele1 is turned off by default
 // RELE2 : GPIO2
 const PROGMEM uint8_t RELE2_PIN = 2;
 boolean m_rele2_state = false; // rele2 is turned off by default
+
+// PIR : GPIO1
+const PROGMEM uint8_t PIR_PIN = 3;
+uint8_t m_pir_state = HIGH; // no motion detected
+uint8_t m_pir_value = 0;
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -90,7 +104,16 @@ void publishReleState() {
   }
 }
 
-// function called to turn on/off the light
+// function called to publish the state of the pir sensor
+void publishPirSensorState() {
+  if (m_pir_state) {
+    client.publish(MQTT_MOTION_STATUS_TOPIC, MOTION_OFF, true);
+  } else {
+    client.publish(MQTT_MOTION_STATUS_TOPIC, MOTION_ON, true);
+  }
+}
+
+// function called to turn on/off the rele
 void setReleState() {
   if (m_rele1_state) {
     digitalWrite(RELE1_PIN, HIGH);
@@ -168,7 +191,7 @@ void reconnect() {
     } else {
       Serial.print("ERROR: failed, rc=");
       Serial.print(client.state());
-      Serial.println("DEBUG: try again in 5 seconds");
+      Serial.println(" DEBUG: try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -183,6 +206,9 @@ void setup() {
   pinMode(RELE1_PIN, OUTPUT);
   pinMode(RELE2_PIN, OUTPUT);
   setReleState();
+
+  // init the PIR
+  pinMode(PIR_PIN,INPUT);
 
   // init the WiFi connection
   Serial.println();
@@ -218,4 +244,21 @@ void loop() {
     reconnect();
   }
   client.loop();
+
+  // read the PIR sensor
+  m_pir_value = digitalRead(PIR_PIN);
+  if (m_pir_value == HIGH) {
+    if (m_pir_state == LOW) {
+      // a motion is detected
+      Serial.println("INFO: Motion detected");
+      publishPirSensorState();
+      m_pir_state = HIGH;
+    }
+  } else {
+    if (m_pir_state == HIGH) {
+      publishPirSensorState();
+      Serial.println("INFO: Motion ended");
+      m_pir_state = LOW;
+    }
+  }
 }
